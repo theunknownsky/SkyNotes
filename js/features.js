0f9c308be065eae73d37a8fb2851df8ea053261f
Vonn Harold Mamaril <vonnmamaril24@gmail.com>
2024-06-07 20:00:46 +0800

    made features.html

---
+++ js/features.js
@@ -0,0 +1,42 @@
+var theme = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Retrieve the variable from sessionStorage
+    const theme = sessionStorage.getItem('theme');
+    // Display the variable
+    console.log('Theme: ' + theme);
+    setFeatureTheme(theme)
+});
+
+// theme change for login.html
+function changeFeatureTheme(){
+    if (theme == 0){
+        changeBodyThemeToLightMode();
+        changeFooterThemeToLightMode();
+        changeHeaderToLightMode();
+        changeFormToLightMode();
+        theme = 1;
+    } else if (theme == 1){
+        changeBodyThemeToDarkMode();
+        changeFooterThemeToDarkMode();
+        changeHeaderToDarkMode();
+        changeFormToDarkMode();
+        theme = 0;
+    }
+    sessionStorage.setItem('theme', theme);
+}
+
+function setFeatureTheme(currentTheme){
+    if (currentTheme == 0){
+        changeBodyThemeToDarkMode();
+        changeFooterThemeToDarkMode();
+        changeHeaderToDarkMode();
+        changeFormToDarkMode();
+    } else if (currentTheme == 1){
+        changeBodyThemeToLightMode();
+        changeFooterThemeToLightMode();
+        changeHeaderToLightMode();
+        changeFormToLightMode();  
+    }   
+    theme = currentTheme;
+}
