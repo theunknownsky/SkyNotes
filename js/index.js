3bbf3ffa56e4e10a9c256dccaab50be2b32002cb
Vonn Harold Mamaril <vonnmamaril24@gmail.com>
2024-06-08 15:53:46 +0800

    removed content in index.html

--- js/index.js
+++ js/index.js
@@ -14,27 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
     setIndexTheme(theme)
 });
 
-// img changes in index.html
-function changeIndexHTMLImgToLightMode(){
-    document.getElementById("theme").src = "../img/sun-black.svg";
-    document.getElementById("secure").src = "../img/shield-check.svg";
-    document.getElementById("eye").src = "../img/eye.svg";
-}
-function changeIndexHTMLImgToDarkMode(){
-    document.getElementById("theme").src = "../img/moon-white.svg";
-    document.getElementById("secure").src = "../img/shield-check-white.svg";
-    document.getElementById("eye").src = "../img/eye-white.svg";
-}
-
 // theme change for index.html
 function changeIndexTheme(){
     if (theme == 0){
-        changeIndexHTMLImgToLightMode();
         changeBodyThemeToLightMode();
         changeFooterThemeToLightMode();
         theme = 1;
     } else if (theme == 1){
-        changeIndexHTMLImgToDarkMode();
         changeBodyThemeToDarkMode();
         changeFooterThemeToDarkMode();
         theme = 0;
@@ -45,11 +31,9 @@ function changeIndexTheme(){
 
 function setIndexTheme(currentTheme){
     if (currentTheme == 0){
-        changeIndexHTMLImgToDarkMode();
         changeBodyThemeToDarkMode();
         changeFooterThemeToDarkMode();
     } else if (currentTheme == 1){
-        changeIndexHTMLImgToLightMode();
         changeBodyThemeToLightMode();
         changeFooterThemeToLightMode();
     }
