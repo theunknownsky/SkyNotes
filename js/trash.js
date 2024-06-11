534eaf5459744682b4df7f7774640b3192ed0ad8
Vonn Harold Mamaril <vonnmamaril24@gmail.com>
2024-06-11 22:05:44 +0800

    archives.html & trash.html content change

---
+++ js/trash.js
@@ -0,0 +1,39 @@
+var theme = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Retrieve the variable from sessionStorage
+    const theme = sessionStorage.getItem('theme');
+    // Display the variable
+    console.log('Theme: ' + theme);
+    setTrashTheme(theme)
+});
+
+// theme change for login.html
+function changeTrashTheme(){
+    if (theme == 0){
+        changeBodyThemeToLightMode();
+        changeNotesHeaderToLightMode();
+        changeNotesNavbarToLightMode();
+        theme = 1;
+    } else if (theme == 1){
+        changeBodyThemeToDarkMode();
+        changeNotesHeaderToDarkMode();
+        changeNotesNavbarToDarkMode();
+        theme = 0;
+    }
+    siteAnimation();
+    sessionStorage.setItem('theme', theme);
+}
+
+function setTrashTheme(currentTheme){
+    if (currentTheme == 0){
+        changeBodyThemeToDarkMode();
+        changeNotesHeaderToDarkMode();
+        changeNotesNavbarToDarkMode();
+    } else if (currentTheme == 1){
+        changeBodyThemeToLightMode();
+        changeNotesHeaderToLightMode();
+        changeNotesNavbarToLightMode();
+    }   
+    theme = currentTheme;
+}
